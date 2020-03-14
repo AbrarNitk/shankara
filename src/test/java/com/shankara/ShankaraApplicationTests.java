2fff56fe0979b1c45e6ed7087b7af57aca97d422
AbrarNitk <abrar.nitk@gmail.com>
2020-03-14 21:37:06 +0530

    init

---
+++ src/test/java/com/shankara/ShankaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShankaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
