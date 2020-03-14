c054a4318887bc6486d325a607cbee8350714711
AbrarNitk <abrar.nitk@gmail.com>
2020-03-14 21:47:08 +0530

    requirements

---
+++ docs/install.md
@@ -0,0 +1,6 @@
+# Install guide
+
+1. Install Java
+2. Install Maven
+3. Install pip
+
--- src/main/java/com/shankara/ShankaraApplication.java
+++ src/main/java/com/shankara/ShankaraApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShankaraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShankaraApplication.class, args);
+	    SpringApplication.run(ShankaraApplication.class, args);
+	    System.out.println("Running Application");
 	}
 
 }
